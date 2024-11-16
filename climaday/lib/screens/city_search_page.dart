036b86655bc46f95c36bb7955463e43153a0a0a4
SavioVitorAlves <saviovitor2@gmail.com>
2024-11-16 16:16:41 -0300

    feat: add widgets of page

--- climaday/lib/screens/city_search_page.dart
+++ climaday/lib/screens/city_search_page.dart
@@ -1,17 +1,61 @@
+import 'package:climaday/components/img_temp.dart';
+import 'package:climaday/components/meteorogical_weather.dart';
 import 'package:flutter/material.dart';
 
-class CitySearchPage extends StatefulWidget {
+class CitySearchPage extends StatelessWidget {
   const CitySearchPage({super.key});
 
   @override
-  State<CitySearchPage> createState() => _CitySearchPageState();
-}
-
-class _CitySearchPageState extends State<CitySearchPage> {
-  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('relatorio da cidade pesquisada!'),
+    Map meteorological = ModalRoute.of(context)!.settings.arguments as Map;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          meteorological["city"] ?? "Cidade Desconhecida",
+          style: const TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      body: Container(
+          padding: const EdgeInsets.all(10),
+          decoration: const BoxDecoration(
+            gradient: RadialGradient(
+              colors: [Color.fromARGB(255, 59, 59, 59), Color(0xFF000000)],
+            ),
+          ),
+          child: Center(
+              child: Column(children: [
+            const SizedBox(
+              height: 5,
+            ),
+            const Text(
+              '11, November de 2024',
+              style: TextStyle(color: Colors.white, fontSize: 12),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            ImgTemp(
+              img: meteorological["condition_slug"],
+              altura: 380,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Text(
+              meteorological["description"],
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold),
+            ),
+            const Spacer(),
+            MeteorogicalWeather(meteorological: meteorological),
+            const SizedBox(
+              height: 20,
+            )
+          ]))),
     );
   }
 }
